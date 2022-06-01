b25d69f6da4d879db49a9b465419e359a1682a51
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-01 16:44:46 +0530

    new commit

---
+++ hamming.rb
@@ -0,0 +1,8 @@
+class Hamming
+  def self.compute(a, b)
+    raise ArgumentError, 'Not the same length.' if a.length != b.length
+    (0...a.length).count do |i|
+      a[i] != b[i]
+    end
+  end
+end
