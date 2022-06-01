9f1ae96a450e43dc8c4c6624debec115a1be1683
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-01 14:59:51 +0530

    new commit

---
+++ wordcount.rb
@@ -0,0 +1,11 @@
+count.rb
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
+  end
+  def word_count
+    @phrase.each_with_object(Hash.new(0)) do |word, hash|
+      hash[word] += 1
+    end
+  end
+end
