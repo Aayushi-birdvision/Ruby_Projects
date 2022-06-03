25c567d2cad40bdf16a810bef3a9ed589c91c132
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-03 14:21:19 +0530

    new commit

--- clock.rb
+++ clock.rb
@@ -1,20 +1,25 @@
 class Clock
   attr_accessor :total_minutes
+
   def initialize(hour: 0, minute: 0)
     @total_minutes = (hour * 60 + minute).modulo(1440)
   end
+
   def to_s
     hours, minutes = total_minutes.divmod(60)
     format('%02d:%02d', hours, minutes)
   end
-  def +(clock)
-    Clock.new(minute: total_minutes + clock.total_minutes)
+
+  def +(other)
+    Clock.new(minute: total_minutes + other.total_minutes)
   end
-  def -(clock)
-    Clock.new(minute: (total_minutes - clock.total_minutes))
+
+  def -(other)
+    Clock.new(minute: (total_minutes - other.total_minutes))
   end
-  def ==(clock)
-    clock.class == self.class &&
-      clock.total_minutes == total_minutes
+
+  def ==(other)
+    other.class == self.class &&
+      other.total_minutes == total_minutes
   end
 end
