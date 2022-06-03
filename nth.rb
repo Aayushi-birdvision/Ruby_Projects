1e55235943ffcbd8bd3d48aaa6591b6bcaf846a7
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-03 13:13:49 +0530

    new commit

---
+++ nth.rb
@@ -0,0 +1,17 @@
+class Prime
+  def self.nth(n)
+    raise ArgumentError if n <= 0
+
+    i = 1
+    c = 0
+    until c == n
+      i += 1
+      c += 1 if prime?(i)
+    end
+    i
+  end
+
+  def self.prime?(n)
+    2.upto(Math.sqrt(n)).all? { |i| n % i != 0 }
+  end
+end
