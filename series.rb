ae5d85473d5b712a34b4d107b15e7d74d2237a60
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-06 15:58:10 +0530

    new commit

--- series.rb
+++ series.rb
@@ -1,16 +1,12 @@
 class Series
-  def initialize(digits)
-    raise ArgumentError unless /^\d*$/ === digits
-    @digits = digits.chars.map(&:to_i)
+  def initialize(string)
+    @string = string
   end
-
-  def largest_product(length)
-    return 1 if length.zero?
-    raise ArgumentError if length.negative? || length > digits.length
-    digits.each_cons(length).map { |arr| arr.reduce(:*) }.max
+  def slices(n)
+    raise ArgumentError if n > @string.length
+    @string         
+      .each_char    
+      .each_cons(n)
+      .map(&:join) 
   end
-  private
-
-  attr_reader :digits
-
 end
