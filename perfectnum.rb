e6b192e09180553c5a9b874a160b564e376f7c0b
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-06-06 17:32:27 +0530

    new commit

---
+++ perfectnum.rb
@@ -0,0 +1,13 @@
+class PerfectNumber
+  def self.classify(number)
+    raise RuntimeError if number.negative?
+    number_factors_sum = (1..number / 2).sum { |n| (number % n).zero? ? n : 0 }
+    if number_factors_sum < number
+      'deficient'
+    elsif number_factors_sum > number
+      'abundant'
+    else
+      'perfect'
+    end
+  end
+end
